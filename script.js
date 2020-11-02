bcc6b6e2957ce18b0fd310fa1d5fc7afae6839da
Sara Peksin <peksin.sara@gmail.com>
2020-11-02 21:51:06 +0100

    api fixes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let result = document.getElementById('result');
 let author = document.getElementById('author');
 function getQuote(){
 /*GET API URL THAT CONVERTES INTO JSON FORMAT*/
-let url="http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+let url="https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
 $.getJSON(url, function (data) {
     $(".result").html(data.quoteText);
     $(".author").html("-" + data.quoteAuthor);
